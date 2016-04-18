533260a9d7dee2718a2b87684b0e5497c131ae2d
Benjie Jiao <hi@benjie.me>
2016-04-19 03:27:21 +0800

    Added /viewer/ URI to main page (quick fix for routing issue)

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ var routes = require('./routes/index');
 
 var app = express();
 
-app.enable('trust proxy');  
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -23,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/viewer', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
