de4d519bb4f83fe406a3c6cdab278ece7c049a57
Benjie Jiao <hi@benjie.me>
2016-05-01 00:48:57 +0800

    Added 12 hour path and current location, now displaying info box

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,21 +1,19 @@
 $(function() {
-    var main_map = L.map('main-map').setView([12.5, 121], 5);
+    var main_map = L.map('main-map').setView([12.5, 121], 3);
 
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         maxZoom: 18,
-        minZoom: 5,
+        minZoom: 3,
         id: 'your.mapbox.project.id',
-        accessToken: 'your.mapbox.public.access.token'
+        accessToken: 'your.mapbox.public.access.token',
     }).addTo(main_map);
 
-    $.getJSON('http://api.orbit.phl-microsat.xyz/diwata1-passes.geojson', function(data){
-        console.log(data);
 
-        for(i=0; i<data.length; i++){
-            data[i].features[0].properties['is_entry_point'] = true
-        }
-        var geoJsonLayer = L.geoJson(data, {
+    $.getJSON('http://api.orbit.phl-microsat.xyz/diwata1-path.geojson?hours=12', function(data){
+        console.log(data);
+        
+        var orbit_path_layer = L.geoJson(data, {
             style: function(feature){
                 if (feature.properties.is_entry_point) {
                     return {color: '#0000FF' }
@@ -25,12 +23,59 @@ $(function() {
                 return new L.CircleMarker(latlng, {radius: 2, fillOpacity: 0.85, color: '#FF0000'});
             },
             onEachFeature: function (feature, layer) {
-                var local_time = moment.tz(feature.properties.timestamp * 1000, "Asia/Manila").format('YYYY-MM-DD HH:mm:SS Z');
+                var local_time = moment.tz(feature.properties.timestamp * 1000, "Asia/Manila").format('YYYY-MM-DD HH:mm:ss Z');
                 layer.bindPopup(local_time);
             }
         });
 
-        main_map.addLayer(geoJsonLayer);
+        main_map.addLayer(orbit_path_layer);
+        main_map.panTo(new L.LatLng(0, data.features[0].geometry.coordinates[0]), {animate: true, duration: 3.0});
+
+    });
+
+
+    $.getJSON('http://api.orbit.phl-microsat.xyz/diwata1-passes.geojson?step=30', function(data){
+        console.log(data);
+
+        for(i=0; i<data.length; i++){
+            data[i].features[0].properties['is_entry_point'] = true
+        }
+        var passes_layer = L.geoJson(data, {
+            style: function(feature){
+                if (feature.properties.is_entry_point) {
+                    return {color: '#0000FF' }
+                }
+            },
+            pointToLayer: function(feature, latlng) {
+                return new L.CircleMarker(latlng, {radius: 2, fillOpacity: 0.85, color: '#FFAAAA'});
+            },
+            onEachFeature: function (feature, layer) {
+                var local_time = moment.tz(feature.properties.timestamp * 1000, "Asia/Manila").format('YYYY-MM-DD HH:mm:ss Z');
+                var time_string = local_time + "<br /> (" + moment(feature.properties.timestamp * 1000).fromNow() + ")"
+                layer.bindPopup(time_string);
+            }
+        });
+
+        main_map.addLayer(passes_layer);
+    });
+
+    $.getJSON('http://api.orbit.phl-microsat.xyz/diwata1', function(original_coordinates){
+        diwata_marker = L.marker([original_coordinates.geometry.coordinates[1], original_coordinates.geometry.coordinates[0]]).addTo(main_map);
+        
+        setInterval(function(){
+            $.getJSON('http://api.orbit.phl-microsat.xyz/diwata1', function(data){
+                diwata_marker.setLatLng(new L.LatLng(data.geometry.coordinates[1], data.geometry.coordinates[0]));
+
+                var infobox_content = "";
+                infobox_content += "time: " + moment.tz(data.properties.timestamp * 1000, "Asia/Manila").format('YYYY-MM-DD HH:mm:ss Z');
+                infobox_content += "<br /> lat: " + data.geometry.coordinates[1];
+                infobox_content += "<br /> lng: " + data.geometry.coordinates[0];
+                infobox_content += "<br /> elevation (m): " + data.properties.elevation;
+
+                $("#info-box").html(infobox_content);
+            });
+        }, 2000);
     });
 });
+
 
