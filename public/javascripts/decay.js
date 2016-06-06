fd4f7b49f611fa13362a05888d1eaeb30a0e3763
Benjie Jiao <hi@benjie.me>
2016-06-07 04:15:31 +0800

    Added working canvasjs graph showing orbital decay fetch from api

--- public/javascripts/decay.js
+++ public/javascripts/decay.js
@@ -1,6 +1,30 @@
 $(function() {
+    var dataPoints = []
     $.getJSON('http://api.orbit.phl-microsat.xyz/decay/diwata1', function(data){
-        console.log(data);
+        for (var i=0; i < data.length; i++) {
+            dataPoints.push({x: moment(data[i].timestamp).toDate(), y: data[i].altitude});
+        }
+        var chart = new CanvasJS.Chart("decay-graph", {
+            zoomEnabled: true,
+            animationEnabled: true,
+            axisX:{
+                labelAngle: 30,
+                labelFontSize: 12,
+                title: "Date (UTCG)",
+                titleFontSize: 14
+            },
+            axisY :{
+                title: "Altitude (km)",
+                titleFontSize: 14
+            },
+            data: [{
+                type: 'line',
+                lineThickness: 2,
+                dataPoints: dataPoints
+            }]
+        });
+        chart.render();
+
     });
 });
-
+          
