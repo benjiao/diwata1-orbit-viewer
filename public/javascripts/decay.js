c4f0354b2f6a284e6857df14ac734722e5f684a5
Benjie Jiao <hi@benjie.me>
2016-06-08 12:48:24 +0800

    Added decay details

--- public/javascripts/decay.js
+++ public/javascripts/decay.js
@@ -1,13 +1,13 @@
 $(function() {
     var dataPoints = []
     $.getJSON('http://api.orbit.phl-microsat.xyz/decay/diwata1', function(results){
-        update_fields(results.meta);
 
         $('.loading').hide();
         $('.main').show();
 
         for (var i=0; i < results.data.length; i++) {
-            dataPoints.push({x: moment(results.data[i].timestamp).toDate(), y: results.data[i].altitude});
+            if (results.data[i].altitude > 120)
+                dataPoints.push({x: moment(results.data[i].timestamp).toDate(), y: results.data[i].altitude});
         }
         var chart = new CanvasJS.Chart("decay-graph", {
             zoomEnabled: true,
@@ -29,17 +29,23 @@ $(function() {
                 dataPoints: dataPoints
             }]
         });
+
+        update_fields(results.meta, last_item=dataPoints[dataPoints.length-1]);
         chart.render();
 
     });
 });
           
-function update_fields(meta){
+function update_fields(meta, last_item){
     $.get('templates/decay-details.html', function(template){
         data = {
-            simulation_time: meta.time_computed,
+            simulation_time: moment.tz(meta.time_computed, "UTC").tz("Asia/Manila").format("D MMMM YYYY HH:mm:ss"),
             tle1: meta.tle1,
-            tle2: meta.tle2
+            tle2: meta.tle2,
+            tle_epoch: moment(meta.tle_epoch).format("D MMMM YYYY HH:mm:ss"),
+            decay_altitude: "120 km above sea level",
+            projected_eol: moment.tz(last_item.x, "UTC").tz("Asia/Manila").format("D MMMM YYYY"),
+            altitude_at_eol: last_item.y
         }
         $('.decay-results-table').html(Mustache.render(template, data));
     })
